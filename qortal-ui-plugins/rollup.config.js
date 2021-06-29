1bce4cb23e00c445d24f791eb9808ecc1395d886
QuickMythril <quickmythril@gmail.com>
2021-06-29 06:17:18 -0400

    updated node-resolve

--- qortal-ui-plugins/rollup.config.js
+++ qortal-ui-plugins/rollup.config.js
@@ -2,7 +2,7 @@
 
 import babel from 'rollup-plugin-babel'
 
-import resolve from 'rollup-plugin-node-resolve'
+import resolve from '@rollup/plugin-node-resolve'
 import builtins from 'rollup-plugin-node-builtins'
 import globals from 'rollup-plugin-node-globals'
 import commonjs from 'rollup-plugin-commonjs'

8f3cf718ddb5f0e8a814ce61688dcfa3370cf582
proto <34919827+protoniuman@users.noreply.github.com>
2021-08-09 20:08:46 +0100

    Fix listing orders on trade portal

--- qortal-ui-plugins/plugins/core/trade-portal/trade-portal.src.js
+++ qortal-ui-plugins/plugins/core/trade-portal/trade-portal.src.js
@@ -1433,9 +1433,7 @@ class TradePortal extends LitElement {
 		const fundingQortAmount = this.round(parseFloat(sellAmountInput) + 0.001) // Set default AT fees for processing to 0.001 QORT // TODO: remove hard-coded values
 
 		const makeRequest = async () => {
-			a
-			const _receivingAddress = null
-
+			let _receivingAddress = null
 			switch (this.selectedCoin) {
 				case 'LITECOIN':
 					_receivingAddress = this.selectedAddress.ltcWallet.address

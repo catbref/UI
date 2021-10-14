f046a68312c6a2a3a85857ec7b625f416ef9dd1e
QuickMythril <quickmythril@gmail.com>
2021-10-14 00:57:53 -0400

    increase version to 1.6.0

--- qortal-ui-core/config/default.user.config.js
+++ qortal-ui-core/config/default.user.config.js
@@ -48,7 +48,7 @@ const user = {
 	nodeSettings: {
 		pingInterval: 10 * 1000, // (10 secs)
 	},
-	version: 'v1.5.0', // TODO: Set this dynamically...
+	version: 'v1.6.0', // TODO: Set this dynamically...
 	language: 'english', // default...english
 	theme: 'light',
 	server: {

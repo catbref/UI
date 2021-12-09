dfb5f1e554869d96a157bb77b51eac4069777fb2
QuickMythril <quickmythril@gmail.com>
2021-12-09 14:59:40 -0500

    increase version to 1.6.1
    new Minting plugin, Send Coin icons, PM button fix

--- qortal-ui-core/config/default.user.config.js
+++ qortal-ui-core/config/default.user.config.js
@@ -47,7 +47,7 @@ const user = {
 	nodeSettings: {
 		pingInterval: 10 * 1000, // (10 secs)
 	},
-	version: 'v1.6.0', // TODO: Set this dynamically...
+	version: 'v1.6.1', // TODO: Set this dynamically...
 	language: 'english', // default...english
 	theme: 'light',
 	server: {
